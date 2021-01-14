1801e1f5085dbaabca9f09e3ab124c372643c2a4
Nurislam Askaruly <51247000+Darwin939@users.noreply.github.com>
2021-01-14 20:08:24 +0600

    add some vield to Apps

--- apps/main/models.py
+++ apps/main/models.py
@@ -12,17 +12,23 @@ class App_Category(models.Model):
     def __str__(self):
         return self.name
 
-#sd
+
 class Apps(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200, blank=True, default='')
     language = models.CharField(blank=True, max_length=100, null=True)
+    language_count = models.IntegerField(blank=True,null=True)
     description = models.TextField(max_length=2000, blank=True, null=True)
     size = models.FloatField(blank=True, null=True)
     award = models.CharField(blank=True, max_length=100, null=True)
     place = models.IntegerField(blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
     category = models.ForeignKey(App_Category, related_name='apps_list', on_delete=models.PROTECT)
+    developer = models.CharField(blank=True, max_length=300, null=True)
+    chart = models.CharField(blank=True, max_length=300, null=True)
+    version = models.CharField(blank=True, max_length=300, null=True)
+    compatibility = models.CharField(blank=True, max_length=400, null=True)
+
 
     class Meta:
         verbose_name = 'Приложения'
