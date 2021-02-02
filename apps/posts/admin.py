afc1150b60a0ebbefacb1b1d92f305d37ee6a8ed
Nurislam Askaruly <51247000+Darwin939@users.noreply.github.com>
2021-02-02 22:50:37 +0600

    added post images to admin panel

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post , Post_Category
+from .models import Post , Post_Category , PostImages
 
 admin.site.register(Post)
 
 admin.site.register(Post_Category)
+
+admin.site.register(PostImages)
